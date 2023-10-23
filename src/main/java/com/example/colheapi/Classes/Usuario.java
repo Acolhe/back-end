f8df3afe02d4c0c0ccfecd499f69e3be6f2d03a5
Matheus Pedrosa Mendes da Costa <matheuscosta-ieg@instituto.ad>
2023-10-23 02:12:45 -0300

    fix: arrumando nmusuario que estava salvando como null

--- src/main/java/com/example/colheapi/Classes/Usuario.java
+++ src/main/java/com/example/colheapi/Classes/Usuario.java
@@ -21,7 +21,8 @@ public class Usuario {
     private long cod_usuario;
 
     @Column(name = "nmusuario", length = 50)
-    private String nm_usuario;
+    @JsonProperty("nmusuario")
+    private String nmusuario;
 
     @Column(name = "dataultimologin")
     private java.util.Date  dataultimologin;
@@ -49,9 +50,9 @@ public class Usuario {
     @Column(name = "premium", columnDefinition = "BOOL DEFAULT FALSE")
     private boolean premium;
 
-    public Usuario(long cod_usuario, String nm_usuario, int saldo, int dias_consecutivos,String email, String senha, Integer cod_skin_principal, Date data_cadastro, boolean premium) {
+    public Usuario(long cod_usuario, String nmusuario, int saldo, int dias_consecutivos,String email, String senha, Integer cod_skin_principal, Date data_cadastro, boolean premium) {
         this.cod_usuario = cod_usuario;
-        this.nm_usuario = nm_usuario;
+        this.nmusuario = nmusuario;
         this.saldo = saldo;
         this.dias_consecutivos = dias_consecutivos;
         this.email = email;
@@ -74,11 +75,11 @@ public class Usuario {
     }
 
     public String getNome() {
-        return nm_usuario;
+        return nmusuario;
     }
 
     public void setNome(String nm_usuario) {
-        this.nm_usuario = nm_usuario;
+        this.nmusuario = nm_usuario;
     }
 
     public int getSaldo() {
@@ -151,7 +152,7 @@ public class Usuario {
     public String toString() {
         return "Usuario{" +
                 "cod_usuario=" + cod_usuario +
-                ", nm_usuario='" + nm_usuario + '\'' +
+                ", nm_usuario='" + nmusuario + '\'' +
                 ", dataultimologin=" + dataultimologin +
                 ", saldo=" + saldo +
                 ", dias_consecutivos=" + dias_consecutivos +
