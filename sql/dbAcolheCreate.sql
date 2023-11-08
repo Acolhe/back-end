30dcf15273b93fd1630cc5768df303150ca9974f
andreasseituno <andre.mendes.germinare@gmail.com>
2023-11-08 18:48:00 -0300

    refactor: Script bd final

--- sql/dbAcolheCreate.sql
+++ sql/dbAcolheCreate.sql
@@ -1,21 +1,8 @@
 -- CREATE DATABASE dbAcolhe
 
 DROP TABLE IF EXISTS Humor;
-DROP TABLE IF EXISTS CompraSkin;
-DROP TABLE IF EXISTS UsuarioClinica;
 DROP TABLE IF EXISTS Clinica;
 DROP TABLE IF EXISTS Usuario;
-DROP TABLE IF EXISTS Skin;
-
-
-CREATE TABLE Skin
-( codSkin   SERIAL 
-, nmSkin    VARCHAR(50)
-, descricao VARCHAR(50)
-, imagem    TEXT
-, valor     INT         DEFAULT(0)
-, PRIMARY KEY (codSkin)
-);
 
 
 CREATE TABLE Usuario
@@ -30,51 +17,26 @@ CREATE TABLE Usuario
 , dataUltimoAcesso DATE        DEFAULT(now())
 , premium          BOOL        DEFAULT(FALSE)
 , PRIMARY KEY (codUsuario)
-, FOREIGN KEY (codSkinPrincipal) REFERENCES Skin (codSkin)
 , UNIQUE (email)
 );
 
 
 CREATE TABLE Clinica
-( codClinica  SERIAL
-, nmClinica   VARCHAR(50)
-, email       VARCHAR(50)
-, telefone    VARCHAR(50)
-, descricao   VARCHAR(125)
-, imagem      TEXT
-, bairro      VARCHAR(50)
-, cidade      VARCHAR(50)
-, nmEstado    VARCHAR(50)
-, sgEstado    VARCHAR(2)
-, patrocinada BOOL        DEFAULT (FALSE)
+( codClinica       SERIAL
+, nmClinica        VARCHAR(50)
+, email            VARCHAR(50)
+, telefone         VARCHAR(50)
+, descricao        VARCHAR(125)
+, imagem           TEXT
+, bairro           VARCHAR(50)
+, cidade           VARCHAR(50)
+, nmEstado         VARCHAR(50)
+, sgEstado         VARCHAR(2)
+, patrocinada      BOOL         DEFAULT (FALSE)
+, nivelSatisfacao  SMALLINT     DEFAULT (3)
 , PRIMARY KEY (codClinica)
 , UNIQUE (email)
 , UNIQUE (telefone)
-);
-
-
-CREATE TABLE UsuarioClinica
-( codUsuarioClinica SERIAL
-, codClinica        INT
-, codUsuario        INT 
-, nivelSatisfacao   SMALLINT 
-, comentario        VARCHAR(125)
-, dataAvaliacao     TIMESTAMP    DEFAULT(now())
-, PRIMARY KEY (codUsuarioClinica)
-, FOREIGN KEY (codClinica) REFERENCES Clinica (codClinica)
-, FOREIGN KEY (codUsuario) REFERENCES Usuario (codUsuario)
-);
-
-
-CREATE TABLE CompraSkin
-( codCompraSkin SERIAL 
-, codSkin       INT
-, codUsuario    INT
-, valor         INT    DEFAULT(0)
-, PRIMARY KEY (codCompraSkin)
-, FOREIGN KEY (codSkin)    REFERENCES Skin    (codSkin)
-, FOREIGN KEY (codUsuario) REFERENCES Usuario (codUsuario)
-, UNIQUE (codSkin, codUsuario)
 );
 
 
@@ -87,4 +49,3 @@ CREATE TABLE Humor
 , PRIMARY KEY (codHumor)
 , FOREIGN KEY (codUsuario) REFERENCES Usuario (codUsuario)
 );
-
